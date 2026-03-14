336f93d057bb9f1298007302c56a4387689ce8de
iJeeSung <kjs2395@gmail.com>
2026-03-13 18:17:04 -0700

    Refactor inline-markdown function

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -10,13 +10,13 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         if len(split_string) % 2 == 0:
             raise Exception(f"Invalid Markdown Syntax: {node.text}")
         substring_list = []
-        for index, substring in enumerate(split_string):
-            if substring == "":
+        for index in range(len(split_string)):
+            if split_string[index] == "":
                 continue
-            if index % 2 != 1:
-                new_node = TextNode(substring, TextType.TEXT)
+            if index % 2 == 0:
+                new_node = TextNode(split_string[index], TextType.TEXT)
             else:
-                new_node = TextNode(substring, text_type)
+                new_node = TextNode(split_string[index], text_type)
             substring_list.append(new_node)
         result.extend(substring_list)
     return result
