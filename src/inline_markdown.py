470b36558d54654e9ef23b93231077fe4a51127a
iJeeSung <kjs2395@gmail.com>
2026-03-16 16:09:16 -0700

    Added split by image and links to inline_markdown.py

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+import re
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     result = []
@@ -20,3 +21,69 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             substring_list.append(new_node)
         result.extend(substring_list)
     return result
+
+def extract_markdown_images(text: str) -> tuple:
+    matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
+
+def extract_markdown_links(text: str) -> tuple:
+    matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
+
+def split_nodes_images(old_nodes: list[TextNode]) -> list[TextNode]:
+    result = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            result.append(node)
+            continue
+        else:
+            list_of_tuples = extract_markdown_images(node.text)
+            if not list_of_tuples:
+                result.append(node)
+                continue
+            else:
+                remaining_text = node.text
+                for image_alt, image_url in list_of_tuples:
+                    sections = remaining_text.split(f"![{image_alt}]({image_url})",1)
+                    if len(sections) != 2:
+                        raise ValueError("invalid markdown, image section not closed")
+                    if sections[0] != "":
+                        result.append(TextNode(sections[0], TextType.TEXT))
+                    result.append(TextNode(image_alt, TextType.IMAGE, image_url))
+                    remaining_text = sections[1]
+                if remaining_text != "":
+                    result.append(TextNode(remaining_text, TextType.TEXT))
+    return result
+
+def split_nodes_links(old_nodes):
+    result = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            result.append(node)
+        else:
+            list_of_tuples = extract_markdown_links(node.text)
+            if not list_of_tuples:
+                result.append(node)
+                continue
+            else:
+                remaining_text = node.text
+                for alt, url in list_of_tuples:
+                    sections = remaining_text.split(f"[{alt}]({url})",1)
+                    if sections[0] != "":
+                        result.append(TextNode(sections[0], TextType.TEXT))
+                    result.append(TextNode(alt, TextType.LINK, url))
+                    remaining_text = sections[1]
+                if remaining_text != "":
+                    result.append(TextNode(remaining_text, TextType.TEXT))
+    return result
+    
+def main():
+    node = TextNode(
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+        TextType.TEXT,
+    )
+    new_node = split_nodes_image([node])
+    print(new_node)
+
+if __name__ == '__main__':
+    main()
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,7 +1,8 @@
 import unittest
 
 from htmlnode import HTMLNode, LeafNode, ParentNode
-from textnode import TextNode
+from textnode import TextNode, TextType
+from inline_markdown import extract_markdown_images, extract_markdown_links, split_nodes_images, split_nodes_links
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
@@ -40,6 +41,59 @@ class TestHTMLNode(unittest.TestCase):
             parent_node.to_html(),
             "<div><span><b>grandchild</b></span></div>",
     )
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images("This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)")
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
 
+    def test_extract_markdown_text(self):
+        matches = extract_markdown_links("This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)")
+        self.assertListEqual( [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
+        
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_images([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+    def test_split_image_single(self):
+        node = TextNode(
+            "![image](https://www.example.COM/IMAGE.PNG)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_images([node])
+        self.assertListEqual(
+            [
+                TextNode("image", TextType.IMAGE, "https://www.example.COM/IMAGE.PNG"),
+            ],
+            new_nodes,
+        )
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with links to [boot.dev](https://boot.dev) and to [Youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_links([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with links to ", TextType.TEXT),
+                TextNode("boot.dev", TextType.LINK, "https://boot.dev"),
+                TextNode(" and to ", TextType.TEXT),
+                TextNode(
+                    "Youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
+                ),
+            ],
+            new_nodes,
+        )
 if __name__ == "__main__":
     unittest.main()
